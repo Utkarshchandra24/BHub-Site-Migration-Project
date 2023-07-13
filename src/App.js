3dbe38534ad183f1a270582c6a5230f6a2cd7e53
Utkarshchandra24 <78264853+Utkarshchandra24@users.noreply.github.com>
2023-07-13 12:40:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import GetOnMobile from './Get On Mobile/GetOnMobile';
+import TodayAttendance from './Today Attendance/TodayAttendance';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <GetOnMobile />
+    </div>
+  );
+}
+
+export default App;
